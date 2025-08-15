fdb62dbdc048624b1a46aa2ee087e08283d10427
Phoebe Jao <jaophoebe@gmail.com>
2025-08-15 13:36:01 +0800

    [fix] Remove motion from pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { motion } from "framer-motion";
 import PageHeader from "../components/block/PageHeader";
 import Timeline from "../components/block/Timeline";
 import img from '/images/about/about_img.png';
@@ -44,64 +43,57 @@ const About = () => {
 
   return (
     <main className='siteContent aboutPage'>
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <section className="aboutSec">
-          <PageHeader
-            title='About'
-            subtitle='My Profile'
-          />
-          <div className="aboutSec__inner">
-            <div className="aboutSec__imgCont">
-              <div className="aboutSec__imgWrap">
-                <img src={img} alt="email" className="aboutSec__img" />
-              </div>
-            </div>
-            <div className="aboutSec__content">
-              <div className="aboutSec__cont">
-                <h2 className="aboutSec__hd fade-up"><span>Junior Frontend Developer</span>Phoebe</h2>
-                <p className="aboutSec__txt fade-up">
-                  Hello, my name is Phoebe Jao, a Junior Frontend Web Developer based in the Philippines. I am passionate about building clean, responsive websites and constantly improving my skills in web development. With a strong foundation in HTML, CSS, and JavaScript, I enjoy turning designs into functional digital experiences. I am eager to learn, grow, and contribute to innovative projects while honing my skills.</p>
-                <div className="aboutSec__socialCont fade-up">
-                  <a href="https://www.linkedin.com/in/phoebe-jao-2b1548195" target="_blank" className="aboutSec__socialBtn">
-                    <img src={linkedin} alt="linkedin" className="aboutSec__socialBtnImg" />
-                  </a>
-                  <a href="https://github.com/Phoebe-Jao" target="_blank" className="aboutSec__socialBtn">
-                    <img src={github} alt="github" className="aboutSec__socialBtnImg" />
-                  </a>
-                </div>
-              </div>
-              <div className="aboutSec__cont">
-                <h3 className="aboutSec__ttl">
-                  <svg>
-                    <text x="0%">Experience</text>
-                  </svg>
-                </h3>
-                <Timeline items={experienceData} />
-              </div>
-              <div className="aboutSec__cont">
-                <h3 className="aboutSec__ttl">
-                  <svg>
-                    <text x="0%">Education</text>
-                  </svg>
-                </h3>
-                <Timeline items={educationData} />
-              </div>
-              <div className="aboutSec__cont u-bdrTrs">
-                <h3 className="aboutSec__ttl">
-                  <svg>
-                    <text x="0%">CV</text>
-                  </svg>
-                </h3>
-              </div>
+      <section className="aboutSec">
+        <PageHeader
+          title='About'
+          subtitle='My Profile'
+        />
+        <div className="aboutSec__inner">
+          <div className="aboutSec__imgCont">
+            <div className="aboutSec__imgWrap">
+              <img src={img} alt="email" className="aboutSec__img" />
             </div>
           </div>
-        </section>
-      </motion.main>
+          <div className="aboutSec__content">
+            <div className="aboutSec__cont">
+              <h2 className="aboutSec__hd fade-up"><span>Junior Frontend Developer</span>Phoebe</h2>
+              <p className="aboutSec__txt fade-up">
+                Hello, my name is Phoebe Jao, a Junior Frontend Web Developer based in the Philippines. I am passionate about building clean, responsive websites and constantly improving my skills in web development. With a strong foundation in HTML, CSS, and JavaScript, I enjoy turning designs into functional digital experiences. I am eager to learn, grow, and contribute to innovative projects while honing my skills.</p>
+              <div className="aboutSec__socialCont fade-up">
+                <a href="https://www.linkedin.com/in/phoebe-jao-2b1548195" target="_blank" className="aboutSec__socialBtn">
+                  <img src={linkedin} alt="linkedin" className="aboutSec__socialBtnImg" />
+                </a>
+                <a href="https://github.com/Phoebe-Jao" target="_blank" className="aboutSec__socialBtn">
+                  <img src={github} alt="github" className="aboutSec__socialBtnImg" />
+                </a>
+              </div>
+            </div>
+            <div className="aboutSec__cont">
+              <h3 className="aboutSec__ttl">
+                <svg>
+                  <text x="0%">Experience</text>
+                </svg>
+              </h3>
+              <Timeline items={experienceData} />
+            </div>
+            <div className="aboutSec__cont">
+              <h3 className="aboutSec__ttl">
+                <svg>
+                  <text x="0%">Education</text>
+                </svg>
+              </h3>
+              <Timeline items={educationData} />
+            </div>
+            <div className="aboutSec__cont u-bdrTrs">
+              <h3 className="aboutSec__ttl">
+                <svg>
+                  <text x="0%">CV</text>
+                </svg>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { motion } from "framer-motion";
 
 import KvSection from '../components/top/KvSection'
 import WorkSection from '../components/top/WorkSection'
@@ -23,17 +22,10 @@ const Top = () => {
 
   return (
     <main className='siteContent topPage'>
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <KvSection />
-        <WorkSection />
-        <AboutSection />
-        <ContactSection />
-      </motion.main>
+      <KvSection />
+      <WorkSection />
+      <AboutSection />
+      <ContactSection />
     </main>
   )
 }
